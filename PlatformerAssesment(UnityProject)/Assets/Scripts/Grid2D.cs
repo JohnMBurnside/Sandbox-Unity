7089408c6df5038fc3b3e789d3db5e53b15e0e89
John Burnside <21.john.burnside@ksd.org>
2019-12-18 13:59:34 -0800

    Update

---
+++ PlatformerAssesment(UnityProject)/Assets/Scripts/Grid2D.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+public class Grid2D : MonoBehaviour
+{
+    #region VARIABLES
+    [Header("Parallax Settings")]
+    public float parallaxScale;
+    [ReadOnly] public Vector3 previousCamPos;
+    Transform cameraTransform;
+    GameObject mainCamera;
+    #endregion
+    #region START FUNCTION
+    void Start()
+    {
+        //PERSPECTIVE CAMERA
+        mainCamera = GameObject.Find("/Player/MainCamera");
+        cameraTransform = Camera.main.transform;
+        previousCamPos = cameraTransform.position;
+    }
+    #endregion
+    #region UPDATE FUNCTION
+    void Update()
+    {
+        //PERSPECTIVE CAMERA
+        float parallax = (previousCamPos.x - cameraTransform.position.x) * parallaxScale;
+        Vector3 pos = transform.position;
+        pos.x += parallax;
+        transform.position = pos;
+        previousCamPos = cameraTransform.position;
+    }
+    #endregion
+}
