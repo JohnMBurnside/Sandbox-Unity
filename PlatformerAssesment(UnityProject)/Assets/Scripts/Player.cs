1b800bdd2b64f0137121039da0684553610968c2
John Burnside <21.john.burnside@ksd.org>
2019-12-19 13:56:55 -0800

    Updates

--- PlatformerAssesment(UnityProject)/Assets/Scripts/Player.cs
+++ PlatformerAssesment(UnityProject)/Assets/Scripts/Player.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 public class Player : MonoBehaviour
 {
     #region VARAIBLES
@@ -12,9 +13,20 @@ public class Player : MonoBehaviour
     [ReadOnly] public int jumpCount;
     Rigidbody2D playerRigidbody;
     [Header("Auto Run Settings")]
+    public bool autoRun;
     public int autoRunSpeed = 10;
     [ReadOnly] public bool autoRunOn;
-    GameObject autoRunText;
+    [ReadOnly] public float textTimer;
+    [ReadOnly] public GameObject autoRunText;
+    [ReadOnly] public GameObject autoRunOffText;
+    [Header("Animation Settings")]
+    Animator animator;
+    [Header("Camera Settings")]
+    [ReadOnly] public GameObject mainCamera;
+    [ReadOnly] public float cameraX;
+    [ReadOnly] public float cameraY;
+    [ReadOnly] public float exitTimer;
+    bool onExit;
     #endregion
     //UNITY FUNCTIONS
     #region START FUNCTION
@@ -26,6 +38,12 @@ public class Player : MonoBehaviour
         //SET AUTO RUN VARIABLES
         autoRunText = GameObject.Find("UI/AutoRunAlerts/AutoRunText");
         autoRunText.SetActive(false);
+        autoRunOffText = GameObject.Find("UI/AutoRunAlerts/AutoRunOffText");
+        autoRunOffText.SetActive(false);
+        //SET ANIMATION VAIABLES
+        animator = GetComponent<Animator>();
+        //SET CAMERA VARIABLES
+        mainCamera = GameObject.Find("Player/MainCamera");
     }
     #endregion
     #region UPDATE FUNCTION
@@ -38,17 +56,33 @@ public class Player : MonoBehaviour
             moveSpeed = 5;
         if (autoRunOn == false)
         {
+            //MOVEMENT
             float moveX = Input.GetAxis("Horizontal");
             Vector2 velocity = GetComponent<Rigidbody2D>().velocity;
             velocity.x = moveSpeed * moveX;
             GetComponent<Rigidbody2D>().velocity = velocity;
+            //ANIMATION
+            animator.SetFloat("x", velocity.x);
+            animator.SetFloat("y", velocity.y);
+            //TIMER
+            textTimer += Time.deltaTime;
+            if (textTimer > 3)
+                autoRunOffText.SetActive(false);
         }
         else
         {
+            //MOVEMENT
             float moveX = 1.0f;
             Vector2 velocity = GetComponent<Rigidbody2D>().velocity;
             velocity.x = autoRunSpeed * moveX;
             GetComponent<Rigidbody2D>().velocity = velocity;
+            //ANIMATION
+            animator.SetFloat("x", velocity.x);
+            animator.SetFloat("y", velocity.y);
+            //TIMER
+            textTimer += Time.deltaTime;
+            if (textTimer > 3)
+                autoRunText.SetActive(false);
         }
         //DOUBLE JUMP AND NORMAL JUMP
         if (Input.GetButtonDown("Jump") && doubleJump == false && grounded == true)
@@ -59,6 +93,21 @@ public class Player : MonoBehaviour
             playerRigidbody.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
         else if (playerRigidbody.velocity.y > 0 && !Input.GetKey(KeyCode.Space))
             playerRigidbody.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
+        //ANIMATION       
+        animator.SetBool("grounded", grounded);
+        float x = Input.GetAxisRaw("Horizontal");
+        if (autoRunOn == true)
+            GetComponent<SpriteRenderer>().flipX = false;
+        else
+        {
+            if (x > 0)
+                GetComponent<SpriteRenderer>().flipX = false;
+            else if (x < 0)
+                GetComponent<SpriteRenderer>().flipX = true;
+        }
+        //EXIT
+        if (onExit == true)
+            OnExit();
     }
     #endregion
     #region ON TRIGGER ENTER 2D FUNCTION
@@ -70,11 +119,38 @@ public class Player : MonoBehaviour
             grounded = true;
             jumpCount = 0;
         }
+        //EXIT TRIGGER
+        if (collision.gameObject.CompareTag("Exit"))
+        {
+            exitTimer = 0;
+            cameraX = mainCamera.GetComponent<Transform>().position.x;
+            cameraY = mainCamera.GetComponent<Transform>().position.y;
+            onExit = true;
+        }
+        //DEATH TRIGGER
+        if (collision.gameObject.CompareTag("Death"))
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         //AUTO RUN
         if (collision.gameObject.CompareTag("AutoRun"))
         {
-            autoRunOn = true;
-            autoRunText.SetActive(true);
+            if(autoRun == true)
+            {
+                if (autoRunOn == false)
+                {
+                    textTimer = 0;
+                    autoRunOn = true;
+                    autoRunText.SetActive(true);
+                }
+            }
+        }
+        if(collision.gameObject.CompareTag("AutoRunOff"))
+        {
+            if(autoRun == true)
+            {
+                textTimer = 0;
+                autoRunOn = false;
+                autoRunOffText.SetActive(true);
+            }
         }
     }
     #endregion
@@ -108,6 +184,15 @@ public class Player : MonoBehaviour
                 jumpCount++;
             jumpCount++;
         }
+    }
+    #endregion
+    #region ON EXIT FUNCTION
+    void OnExit()
+    {
+        exitTimer += Time.deltaTime;
+        mainCamera.GetComponent<Transform>().position = new Vector3(cameraX, cameraY, -10);
+        if (exitTimer > 3)
+            SceneManager.LoadScene("LevelTwo");
     }
     #endregion
 }
