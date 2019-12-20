87a76ac51714e5cbb1c84722103229138e9c69b1
John Burnside <21.john.burnside@ksd.org>
2019-12-20 13:52:46 -0800

    Finished Platformer Assesment Project

---
+++ WizardGoesIntoDungeon_TheGame(UnityProject)/Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+public class EnemyScript : MonoBehaviour
+{
+    #region VARIABLES
+    [Header("Movement Variables")]
+    public Vector2 moveDir;
+    public float moveSpeed = 3.0f;
+    public float paceDuration = 3.0f;
+    public float timer = 0;
+    #endregion
+    //UNITY FUNCTIONS
+    #region START FUNCTION
+    void Start()
+    {
+        //SET MOVEMENT VARIABLES
+        moveDir.Normalize();
+    }
+    #endregion
+    #region UPDATE FUNCTION
+    void Update()
+    {
+        //MOVEMENT
+        timer += Time.deltaTime;
+        if (timer >= paceDuration)
+        {
+            moveDir *= -1;
+            timer = 0;
+        }
+        GetComponent<Rigidbody2D>().velocity = moveDir * moveSpeed;
+        //ANIMATION
+        Vector2 velocity = GetComponent<Rigidbody2D>().velocity;
+        velocity.Normalize();
+        GetComponent<Animator>().SetFloat("x", velocity.x);
+        GetComponent<Animator>().SetFloat("y", velocity.y);
+    }
+    #endregion
+}
---
+++ WizardGoesIntoDungeon_TheGame(UnityProject)/Assets/Scripts/UIScript.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+public class UIScript : MonoBehaviour
+{
+    #region VARIABLES
+    [Header("General Variables")]
+    public Animator animator;
+    string levelToLoad;
+    GameObject player;
+    #endregion
+    //UNITY FUNCTIONS
+    #region START FUNCTION
+    void Start()
+    {
+        try
+        {
+            animator = GetComponent<Animator>();
+        }
+        finally
+        {
+            Debug.Log("Animator Not Found");
+            animator = null;
+        }
+        try
+        {
+            player = GameObject.Find("Player/");
+        }
+        finally
+        {
+            Debug.Log("Player Not Found");
+            player = null;
+        }
+    }
+    #endregion
+    #region UPDATE FUNCTION
+    void Update()
+    {
+        if (player.GetComponent<PlayerScript>().loadLevel == true)
+            FadeToLevel("LevelTwo");
+    }
+    #endregion
+    //UI FUNCTIONS
+    #region START GAME FUNCTION
+    public void StartGame()
+    {
+        SceneManager.LoadScene("LevelOne");
+    }
+    #endregion
+    #region QUIT FUNCTION
+    public void Quit()
+    {
+        Application.Quit();
+    }
+    #endregion
+    #region FADE TO LEVEL FUNCTION
+    public void FadeToLevel(string levelName)
+    {
+        levelToLoad = levelName;
+        animator.SetTrigger("FadeOut");
+    }
+    #endregion
+    #region ON FADE COMPLETE FUNCTIOn
+    public void OnFadeComplete()
+    {
+        SceneManager.LoadScene(levelToLoad);
+    }
+    #endregion
+}
