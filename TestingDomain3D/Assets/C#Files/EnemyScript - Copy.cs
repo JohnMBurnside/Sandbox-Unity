1d1728bd477652072b2b6425aa308c24af0a8bf5
JohnMBurnside <JohnMBurnside21@gmail.com>
2020-04-28 21:48:45 -0700

    Updates

---
+++ TestingDomain3D/Assets/C#Files/EnemyScript - Copy.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+public class EnemyScript : MonoBehaviour
+{
+    #region VARIABLES
+    public float health = 50f;
+    #endregion
+    //ENEMY FUNCTIONS
+    #region TAKE DAMAGE FUNCTION
+    public void TakeDamage(float damage)
+    {
+        health -= damage;
+        if (health <= 0f)
+            Destroy(gameObject);
+    }
+    #endregion
+}
